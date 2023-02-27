6b633e9c5987e2d6401e14e2669879044b9375ea
zheniaStukalo <82517499+t0xick1d@users.noreply.github.com>
2023-02-27 17:13:22 +0200

    create rep, and add need fiels

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import ContactForm from './ContactForm/ContactForm';
+import NumberList from './NumberList/NumberList';
+import Filter from './Filter/Filter';
+
+import style from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addNumber = objNumber => {
+    const { id, name, number } = objNumber;
+    if (this.state.contacts.filter(e => e.name === name).length !== 0) {
+      alert(`${name}is already in contacts.`);
+      return;
+    }
+    this.setState(prevState => {
+      return {
+        contacts: [{ id, name, number }, ...prevState.contacts],
+      };
+    });
+  };
+
+  onFilterChage = e => {
+    const value = e.currentTarget.value;
+    this.setState({ filter: value });
+  };
+
+  deleteContact = id => {
+    const newListContact = this.state.contacts.filter(
+      contact => contact.id !== id
+    );
+    this.setState({ contacts: newListContact });
+  };
+
+  render() {
+    const normilizeFilter = this.state.filter.toLowerCase();
+    const visibleContact = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normilizeFilter)
+    );
+
+    return (
+      <div className={style.App__container}>
+        <div>
+          <h1 className={style.App__title}>Phonebook</h1>
+          <ContactForm addNumber={this.addNumber} />
+          <h2 className={style.App__title}>Contacts</h2>
+          <Filter
+            filter={this.state.filter}
+            onFilterChage={this.onFilterChage}
+          />
+          <NumberList
+            list={visibleContact}
+            deleteContact={this.deleteContact}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NumberList/ItemList.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import style from './NumberList.module.css';
+
+function ItemList({ id, name, number, deleteContact }) {
+  return (
+    <li key={id} className={style.item}>
+      {name}, {number}{' '}
+      <button
+        className={style.button__delete}
+        onClick={() => {
+          deleteContact(id);
+        }}
+      >
+        {' '}
+        Delete{' '}
+      </button>
+    </li>
+  );
+}
+
+export default ItemList;
+
+ItemList.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  deleteContact: PropTypes.func.isRequired,
+};
